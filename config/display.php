ead0795d781b9315a2ac757cdc42a2beba8d0fd7
Francis Beam Santander <santanderfrancisbeam@gmail.com>
2022-08-03 20:15:37 +0800

    debugging Inventory

--- config/display.php
+++ config/display.php
@@ -6,11 +6,13 @@ if(isset($_POST['displaySend'])){
       <tr>
         <th scope="col">No.</th>
         <th scope="col">Item name</th>
+        <th scope="col">Category</th>
         <th scope="col">Unit price</th>
         <th scope="col">Stock</th>
         <th scope="col">Status</th>
         <th scope="col">Sales</th>
         <th scope="col">Return</th>
+        <th scope="col">Supplier</th>
         <th scope="col">Action</th>
       </tr>
     </thead>';
@@ -20,19 +22,21 @@ if(isset($_POST['displaySend'])){
     while($row=mysqli_fetch_assoc($result)){
         $id=$row['id'];
         $itemname=$row['itemname'];
-        $unitprice=$row['unitprice'];
+        $category=$row['category'];
+        $unitcost=$row['unitcost'];
         $stock=$row['stock'];
         $status=$row['status'];
-        $sales=$row['sales'];
-        $return=$row['return'];
+        $supplier=$row['supplier'];
+        // $productimage=$row['productimage'];
+        // $description=$row['productdescription'];
         $table.='<tr>
         <td scope="row">'.$number.'</td>
         <td>'.$itemname.'</td>
-        <td>'.$unitprice.'</td>
+        <td>'.$category.'</td>
+        <td>'.$unitcost.'</td>
         <td>'.$stock.'</td>
         <td>'.$status.'</td>
-        <td>'.$sales.'</td>
-        <td>'.$return.'</td>
+        <td>'.$supplier.'</td>
         <td>
         <button class = "btn btn-primary" onclick="updateProduct('.$no.')">Update</button>
         <button class = "btn btn-danger" onclick="deleteProduct('.$no.')">Delete</button>
--- pages/administrator/inventory.php
+++ pages/administrator/inventory.php
@@ -17,10 +17,13 @@
 
                     <!-- TOP FUNCTION -->
             <div class="top-function">
+
+              <!-- BUTTON FOR ADD ITEM -->
                <button type="button" class="btn btn-primary mx-5 mt-3 my-3" data-toggle="modal" data-target="#completeModal">
             Add New Product
                 </button>
 
+                     <!-- MODAL FOR ADD ITEM -->
                 <div class="modal fade" id="completeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -35,11 +38,11 @@
                     <div class="form-group">
                     <div class="form-group">
                         <label for="nameProduct mb-3">Item name</label>
-                        <input type="text" class="form-control mb-3 border" id="completename" placeholder="Name of the product">
+                        <input type="text" class="form-control mb-3 border" id="itemnamefield" placeholder="Name of the product">
                         </div>
 
                     <label for="foodCategory mb-3">Category</label>
-                    <select class="form-control mb-3 border" id="completecategory">
+                    <select class="form-control mb-3 border" id="categoryfield">
                         <option>Hand Tools</option>
                         <option>Welding Equipment</option>
                         <option>Paints</option>
@@ -54,15 +57,15 @@
                 
                     <div class="form-group mb-3">
                         <label for="nameProduct mb-3">Unit Price ₱</label>
-                        <input type="number" class="form-control mb-3 border" id="completeprice" >
+                        <input type="number" class="form-control mb-3 border" id="unitcostfield" >
                         </div>
                    
                     <div class="input-group mb-3"> 
                     <div class="input-stock-prepend">
                         <span class="input-group-text">Stock</span>
                     </div>
-                    <input type="number" class="form-control border" id="completestock" style="border-radius: 5px black;">
-                    <select class="status-prepend" id="completestatus">
+                    <input type="number" class="form-control border" id="stockfield" style="border-radius: 5px black;">
+                    <select class="status-prepend" id="statusfield">
                     <option class="badge bg-success">Available</option>
                         <option class="badge bg-warning">Low</option>
                         </select>
@@ -70,18 +73,18 @@
 
                     <div class="form-group mb-3">
                         <label for="nameProduct mb-3">Supplier Name</label>
-                        <input type="text" class="form-control mb-3 border" id="completesupplier" >
+                        <input type="text" class="form-control mb-3 border" id="supplierfield" >
                         </div>
 
                         
                         <div class="form-group mb-3">
                             <label for="nameProduct mb-3">Product Image</label>
-                            <input type="file" class="form-control mb-3 border" id="img" name="img" accept="image/*">
+                            <input type="file" class="form-control mb-3 border" id="imagefield" name="img" accept="image/*">
                             </div>
                             
                         <div class="form-group mb-3">
-                            <label for="nameProduct mb-3">Product Description</label>
-                           <textarea name="productdescription" id="" cols="30" rows="5" style="resize: none;"></textarea>
+                            <label for="productDescription mb-3">Product Description</label>
+                           <textarea name="descriptionfield" id="" cols="30" rows="5" style="resize: none;"></textarea>
                             </div>
 
                     </div>
@@ -96,14 +99,93 @@
                 </div>
                 </div>
 
-                    <div class="status">
-                        <label class="status">Status</label>
-                        <select name="status" id="">
-                                <option value="available">Available</option>
-                                <option value="available">Low</option>
-                                <option value="available">Out of stock</option>
+            <!-- update modal -->
+            <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                 
+                    <div class="form-group">
+                    <div class="form-group">
+                        <label for="nameProduct mb-3">Item name</label>
+                        <input type="text" class="form-control mb-3 border" id="updateitemnamefield" placeholder="Name of the product">
+                        </div>
+
+                    <label for="foodCategory mb-3">Category</label>
+                    <select class="form-control mb-3 border" id="updatecategoryfield">
+                        <option>Hand Tools</option>
+                        <option>Welding Equipment</option>
+                        <option>Paints</option>
+                        <option>Cement</option>
+                        <option>Woods</option>
+                        <option>Cutting Tools</option>
+                        <option>Power Tools</option>
+                        <option>Structural Tools</option>
+                        <option>Measure Tools</option>
+                    </select>
+                    </div>
+                
+                    <div class="form-group mb-3">
+                        <label for="nameProduct mb-3">Unit Price ₱</label>
+                        <input type="number" class="form-control mb-3 border" id="updateunitcostfield" >
+                        </div>
+                   
+                    <div class="input-group mb-3"> 
+                    <div class="input-stock-prepend">
+                        <span class="input-group-text">Stock</span>
+                    </div>
+                    <input type="number" class="form-control border" id="updatestockfield" style="border-radius: 5px black;">
+                    <select class="status-prepend" id="updatestatusfield">
+                    <option class="badge bg-success">Available</option>
+                        <option class="badge bg-warning">Low</option>
                         </select>
                     </div>
+
+                    <div class="form-group mb-3">
+                        <label for="nameProduct mb-3">Supplier Name</label>
+                        <input type="text" class="form-control mb-3 border" id="updatesupplierfield" >
+                        </div>
+
+                        
+                        <div class="form-group mb-3">
+                            <label for="nameProduct mb-3">Product Image</label>
+                            <input type="file" class="form-control mb-3 border" id="updateimagefield" name="img" accept="image/*">
+                            </div>
+                            
+                        <div class="form-group mb-3">
+                            <label for="productDescription mb-3">Product Description</label>
+                           <textarea name="descriptionfield" id="updatedescriptionfield" cols="30" rows="5" style="resize: none;"></textarea>
+                            </div>
+
+                    </div>
+
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" onclick="updateDetails()">Update</button>
+
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <input type="hidden" id="hiddendata">
+                       </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- SORT STATUS -->
+                    <div class="status">
+                        <label class="status">Status</label>
+                        <select name="status" id="">                     
+                                <option value="available">Available</option>
+                                <option value="low">Low</option>
+                                <option value="outstock">Out of stock</option>
+                        </select>
+                    </div>
+
+                    <!-- SEARCH ITEM -->
                     <div class="item-name">
                         <label class="itemName">Item name</label>
                         <input type="text">
@@ -111,12 +193,20 @@
             </div>
 
             <!-- INVENTORY TABLE -->
-            <div class="inventory-container">
+            <!-- <div class="inventory-container">
                 <div id="result"></div>
-            </div>
+            </div> -->
+
+            <div id="result"></div>
+
+                 <!-- <div id="displayDataTable"></div> -->
     </section>
 
     <script src="/assets/js/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="/js/product.js"></script>
 
 </body>
 </html>
